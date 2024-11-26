4af86d778bf86bb44f8375975d3da8506f6e56bd
Владислав Кошелев <testvlad59@gmail.com>
2024-11-27 00:19:15 +0300

    реализованы контроллеры страниц home.html и contacts.html

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,8 +3,7 @@ from django.urls import path
 from config.urls import urlpatterns
 from . import views
 
-
 urlpatterns = [
     path('home/', views.home(), name="home"),
-    path('contacts/', views.contacts(), name="contacts")
+    path('contacts/', views.contacts(), name="contacts"),
 ]
