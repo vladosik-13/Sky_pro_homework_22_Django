174cd16cf28b81884ff556ca0de8c4ae59ed5375
Владислав Кошелев <testvlad59@gmail.com>
2024-11-26 23:47:36 +0300

    объявлен список urlpatterns

---
+++ catalog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from config.urls import urlpatterns
+from . import views
+
+
+urlpatterns = [
+    path('home/', views.home(), name="home"),
+    path('contacts/', views.contacts(), name="contacts")
+]
