86bdc5e7580381d5039a1d557079ca97b1f8cbf1
Владислав Кошелев <testvlad59@gmail.com>
2024-12-04 00:56:14 +0300

    Зарегистрированы модели Product и Category в админке. для моделей настроено отображение согласно ТЗ

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Product, Category
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name', 'description')
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description', 'price', 'category')
+    search_fields = ('name', 'description')
+    list_filter = ('category',)
