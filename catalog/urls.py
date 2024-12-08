4c4ee7e3e0cc6bec2c1e835853ef852ac5c032d2
Владислав Кошелев <testvlad59@gmail.com>
2024-12-09 02:34:17 +0300

    Реализован контроллер и шаблон для отображения страницы с подробной информацией о товаре

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from catalog.views import product_list
+from catalog.views import product_list, product_details
 
 
 app_name = "catalog"
@@ -8,5 +8,6 @@ app_name = "catalog"
 urlpatterns = [
     path("home/", views.home, name="home"),
     path("contacts/", views.contacts, name="contacts"),
-    path ('', product_list, name="product_list"),
+    path("", product_list, name="product_list"),
+    path('product/<int:pk>/', product_details, name="product_details"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from itertools import product
+from lib2to3.fixes.fix_input import context
+
+from django.shortcuts import render, get_object_or_404
 
 from catalog.models import Product
 
@@ -15,3 +18,9 @@ def product_list(request):
     products = Product.objects.all()
     context = {"products":  products}
     return render(request, "catalog/product_list.html", context)
+
+
+def product_details(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {"product": product}
+    return render(request, "catalog/product_details.html", context)
