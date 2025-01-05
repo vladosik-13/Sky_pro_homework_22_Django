6f8c11acdded22f113cefaeeb630b2e877a69665
Владислав Кошелев <testvlad59@gmail.com>
2025-01-06 01:33:16 +0300

    переделаны шаблоны и views.py

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path
-from blogs.apps import BlogsConfig
+from .views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView
 
-from blogs.views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView
-
-app_name = BlogsConfig.name
+app_name = 'blogs'
 
 urlpatterns = [
-    path('', BlogListView.as_view(), name='home'),
-    path('article/<int:pk>/', BlogDetailView.as_view(), name='article_detail'),
-    path('article/create/', BlogCreateView.as_view(), name='article_create'),
-    path('article/<int:pk>/update/', BlogUpdateView.as_view(), name='article_update'),
-    path('article/<int:pk>/delete/', BlogDeleteView.as_view(), name='article_delete'),
+    path('', BlogListView.as_view(), name='blog_list'),
+    path('<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
+    path('create/', BlogCreateView.as_view(), name='blog_create'),
+    path('<int:pk>/update/', BlogUpdateView.as_view(), name='blog_update'),
+    path('<int:pk>/delete/', BlogDeleteView.as_view(), name='blog_delete'),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,28 +1,46 @@
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
-from blogs.models import Blog
+from .models import Blog
 
 
 class BlogListView(ListView):
     model = Blog
+    template_name = 'blogs/blog_list.html'
+    context_object_name = 'object_list'  # По умолчанию используется 'object_list', но можно явно указать
 
 
 class BlogDetailView(DetailView):
     model = Blog
+    template_name = 'blogs/blog_detail.html'
+    context_object_name = 'object'  # По умолчанию используется 'object', но можно явно указать
 
 
 class BlogCreateView(CreateView):
     model = Blog
     fields = ['title', 'content']
-    success_url = reverse_lazy('blog_list')
+    success_url = reverse_lazy('blogs:blog_list')
+    template_name = 'blogs/blog_form.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Создание нового блога'
+        return context
 
 
 class BlogUpdateView(UpdateView):
     model = Blog
     fields = ['title', 'content']
-    success_url = reverse_lazy('blog_list')
+    success_url = reverse_lazy('blogs:blog_list')
+    template_name = 'blogs/blog_form.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Редактирование блога'
+        return context
+
 
 class BlogDeleteView(DeleteView):
     model = Blog
-    success_url = reverse_lazy('blog_list')
+    success_url = reverse_lazy('blogs:blog_list')
+    template_name = 'blogs/blog_confirm_delete.html'
