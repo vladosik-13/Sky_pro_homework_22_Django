c053bddeeeb2106dc5f3d22afe9a9a59e73956d6
Владислав Кошелев <testvlad59@gmail.com>
2025-01-05 15:57:10 +0300

    Доделан CRUD в модуле views.py в полном объеме

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from blogs.apps import BlogsConfig
 
-from blogs.views import BlogListView, BlogDetailView, BlogCreateView
-
+from blogs.views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView
 
 app_name = BlogsConfig.name
 
@@ -10,4 +9,6 @@ urlpatterns = [
     path('', BlogListView.as_view(), name='blog_list'),
     path('<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
     path('create/', BlogCreateView.as_view(), name='blog_create'),
+    path('<int:pk>/update/', BlogUpdateView.as_view(), name='blog_update'),
+    path('<int:pk>/delete/', BlogDeleteView.as_view(), name='blog_delete'),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,5 @@
 from django.urls import reverse_lazy
-from django.views.generic import DetailView, CreateView
+from django.views.generic import DetailView, CreateView, UpdateView, DeleteView
 
 from blogs.models import Blog
 
@@ -16,4 +16,13 @@ class BlogCreateView(CreateView):
     model = Blog
     fields = ['title', 'content']
     success_url = reverse_lazy('blog_list')
-    
+
+
+class BlogUpdateView(UpdateView):
+    model = Blog
+    fields = ['title', 'content']
+    success_url = reverse_lazy('blog_list')
+
+class BlogDeleteView(DeleteView):
+    model = Blog
+    success_url = reverse_lazy('blog_list')
