e9157cd3a24622e4e31d24ff64c3e84eeb5ae503
Владислав Кошелев <testvlad59@gmail.com>
2025-01-07 02:35:22 +0300

    реализована стилизация форм для продуктов

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.core.exceptions import ValidationError
-from catalog.models import Product
+from catalog.models import Product, Category
 
 
 class ProductForm(forms.ModelForm):
@@ -29,3 +29,11 @@ class ProductForm(forms.ModelForm):
         if price is not None and price < 0:
             raise ValidationError('Цена не может быть отрицательной.')
         return price
+
+    def __init__(self, *args, **kwargs):
+        super(ProductForm, self).__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs.update({'class': 'form-control', 'placeholder': 'введите наименование продукта'})
+        self.fields['description'].widget.attrs.update({'class': 'form-control', 'placeholder': 'введите описание продукта'})
+        self.fields['price'].widget.attrs.update({'class': 'form-control', 'placeholder': 'введите цену'})
+        self.fields['image'].widget.attrs.update({'class': 'form-control'})
+        self.fields['category'].widget.attrs.update({'class': 'form-control'})
