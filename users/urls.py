804af4b57445bb814d05efdcacf62aa07efc9358
Владислав Кошелев <testvlad59@gmail.com>
2025-01-09 01:44:17 +0300

    подправлен модкль urls.py

--- users/urls.py
+++ users/urls.py
@@ -8,5 +8,5 @@ app_name = 'users'
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(template_name = 'user/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(next_page = 'catalog:product_list'), name='logout')
+    path('logout/', LogoutView.as_view(next_page = 'user/logout'), name='logout')
 ]
