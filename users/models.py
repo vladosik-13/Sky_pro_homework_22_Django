152a16a3578a1b67e46a90f5be0800989fe32a57
Владислав Кошелев <testvlad59@gmail.com>
2025-01-09 01:12:05 +0300

    Исправлена ошибка в приложении users. были проблемы с миграциями

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
+# users/models.py
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from .managers import CustomUserManager
 
 class CustomUser(AbstractUser):
     username_validator = None
@@ -9,8 +11,10 @@ class CustomUser(AbstractUser):
     avatar = models.ImageField(upload_to='avatars', blank=True, null=True, verbose_name=_('Avatar'))
     country = models.CharField(max_length=50, blank=True, null=True, verbose_name=_('Country'))
 
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []  # Убрано поле 'username'
+
+    objects = CustomUserManager()
 
     def __str__(self):
         return self.email
