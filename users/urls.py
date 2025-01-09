c30531b8bc60d8571beee1e7c923ccc22af8ce8d
Владислав Кошелев <testvlad59@gmail.com>
2025-01-09 22:57:42 +0300

    подправлена маршрутизация для login

--- users/urls.py
+++ users/urls.py
@@ -6,6 +6,6 @@ app_name = 'users'
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('login/', LoginView.as_view(template_name='users/login.html', next_page='catalog:product_list'), name='login'),
     path('logout/', LogoutView.as_view(next_page='catalog:product_list'), name='logout'),
 ]
