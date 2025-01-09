2521db1323fdc8d82cf61f38c90cb6ebcb278bf5
Владислав Кошелев <testvlad59@gmail.com>
2025-01-09 22:51:43 +0300

    Имправлен inc_menu.html чтобы logout работал корректно

--- users/views.py
+++ users/views.py
@@ -8,7 +8,7 @@ from django.core.mail import send_mail
 class RegisterView(CreateView):
     template_name = 'users/register.html'
     form_class = CustomUserCreationForm
-    success_url = reverse_lazy('users:login')
+    success_url = reverse_lazy('catalog:product_list')
 
     def form_valid(self, form):
         user = form.save()
