b5c728e9a993d5c4a6717ba7c400bddc623fa4b9
Владислав Кошелев <testvlad59@gmail.com>
2025-01-12 22:18:27 +0300

    исправление для админки

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,26 @@
+# users/admin.py
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _  # Добавьте этот импорт
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(BaseUserAdmin):
+    model = CustomUser
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'phone_number', 'avatar', 'country')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'first_name', 'last_name', 'phone_number', 'avatar', 'country', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+    filter_horizontal = ('groups', 'user_permissions',)
+
+admin.site.register(CustomUser, CustomUserAdmin)
