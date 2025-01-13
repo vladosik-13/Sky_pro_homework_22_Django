0d42d2cd3093a976ec672c29d3a6f2f132d91ae0
Владислав Кошелев <testvlad59@gmail.com>
2025-01-14 00:34:25 +0300

    переделан модуль admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,17 +1,16 @@
-# users/admin.py
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.utils.translation import gettext_lazy as _  # Добавьте этот импорт
 from .models import CustomUser
 
+@admin.register(CustomUser)
 class CustomUserAdmin(BaseUserAdmin):
     model = CustomUser
     list_display = ('email', 'first_name', 'last_name', 'is_staff')
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        (_('Personal info'), {'fields': ('first_name', 'last_name', 'phone_number', 'avatar', 'country')}),
-        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'phone_number', 'avatar', 'country')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
     )
     add_fieldsets = (
         (None, {
@@ -22,5 +21,3 @@ class CustomUserAdmin(BaseUserAdmin):
     search_fields = ('email', 'first_name', 'last_name')
     ordering = ('email',)
     filter_horizontal = ('groups', 'user_permissions',)
-
-admin.site.register(CustomUser, CustomUserAdmin)
